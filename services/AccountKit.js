b80196d5da3ea2d086ed356e394d55c300b39a15
ngthorg <ngthorg@gmail.com>
2016-07-13 23:15:05 +0700

    mv request to Axios

--- services/AccountKit.js
+++ services/AccountKit.js
@@ -1,66 +1,43 @@
-const request = require('request');
-const Querystring = require('querystring');
-const crypto = require('crypto');
-
 // import Guid from 'guid';
-import { facebook as FacebookConfig } from '../config';
+import { facebook } from '../config';
+const Axios = require('axios');
+const crypto = require('crypto');
 
 
 // const csrf_guid = Guid.raw();
 // const apiVersion = 'v1.0';
-const APPID = FacebookConfig.app_id;
-const APPSECRET = FacebookConfig.app_secret;
+const APPID = facebook.app_id;
+const APPSECRET = facebook.app_secret;
 const ME_ENDPOINT_BASE_URL = 'https://graph.accountkit.com/v1.0/me';
 const TOKEN_EXCHANGE_BASE_URL = 'https://graph.accountkit.com/v1.0/access_token';
 
 export const foo = 'foo';
 
 export function sendcode(code) {
-  return new Promise((resolve, reject) => {
-    // if (csrfNonce !== csrf_guid) return reject('Something went wrong.');
-    const appAccessToken = ['AA', APPID, APPSECRET].join('|');
+  const appAccessToken = ['AA', APPID, APPSECRET].join('|');
 
-    const params = {
+  return Axios.get(TOKEN_EXCHANGE_BASE_URL, {
+    params: {
       grant_type: 'authorization_code',
       code,
       access_token: appAccessToken,
-    };
+    },
+  }).then(respBody => {
+    const hash = crypto.createHmac('sha256', APPSECRET)
+      .update(respBody.access_token)
+      .digest('hex');
 
-    // exchange tokens
-    const tokenExchangeUrl = TOKEN_EXCHANGE_BASE_URL + '?' + Querystring.stringify(params);
-
-    request.get({
-      url: tokenExchangeUrl,
-      json: true,
-    }, (err, resp, respBody) => {
-      if (err) reject(err);
-      else if (respBody.error) reject(respBody.error);
-
-      const hash = crypto.createHmac('sha256', APPSECRET)
-        .update(respBody.access_token)
-        .digest('hex');
-
-      // get account details at /me endpoint
-      const me_endpoint_url = ME_ENDPOINT_BASE_URL
-        + '?access_token=' + respBody.access_token +
-        '&appsecret_proof=' + hash;
-
-      request.get({ url: me_endpoint_url, json: true }, (err, resp, respBody) => {
-        if (err) {
-          reject(err);
-        } else if (respBody.error) {
-          reject(respBody.error);
-        }
-
-        // if (respBody.phone) {
-        //   res.phone_num = respBody.phone.number;
-        // }
-        // else if (respBody.email) {
-        //   res.email_addr = respBody.email.address;
-        // }
-
-        resolve(respBody);
-      });
+    return Axios.get(ME_ENDPOINT_BASE_URL, {
+      params: {
+        access_token: respBody.access_token,
+        appsecret_proof: hash,
+      },
+    }).then(res => res)
+    .catch(err => {
+      throw err.data.error;
     });
+  })
+  .catch(err => {
+    throw err.data.error;
   });
 }
